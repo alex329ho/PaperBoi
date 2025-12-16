e1b5eb9166bd785947a9ab59fdc5e4e7c2e54a24
Ho Alex <alex329ho@gmail.com>
2025-12-17 02:29:47 +0800

    update

--- mobile/.husky/_/husky.sh
+++ mobile/.husky/_/husky.sh
@@ -1,19 +1,9 @@
-#!/bin/sh
-if [ -z "$husky_skip_init" ]; then
-  husky_skip_init=1
-  debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
-  }
-  readonly husky_skip_init
-  export husky_skip_init
-  debug "starting" "$0"
-  if [ "$HUSKY" = "0" ]; then
-    debug "HUSKY env variable is set to 0, skipping hook"
-    exit 0
-  fi
-  if [ -f ~/.huskyrc ]; then
-    debug "sourcing ~/.huskyrc"
-    . ~/.huskyrc
-  fi
-  export readonly husky_skip_init
-fi
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
